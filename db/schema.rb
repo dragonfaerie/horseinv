97cc75b8871a0586bb0f7d457e859c3a71534852
dragonfaerie <hope.m.ross@gmail.com>
2023-06-03 15:51:17 -0500

    added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_20_163302) do
+ActiveRecord::Schema.define(version: 2023_06_03_204619) do
 
   create_table "horses", force: :cascade do |t|
     t.integer "make"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2023_05_20_163302) do
     t.string "office_pony"
     t.integer "purchase_price"
     t.string "condition"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
