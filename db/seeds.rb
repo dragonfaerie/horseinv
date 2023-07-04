4358870bc3c57e857ff4138b53be992ab06a9965
dragonfaerie <hope.m.ross@gmail.com>
2023-07-04 15:43:56 -0500

    updating for deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create(name: 'Hope', email: 'kitszoo@gmail.com', username: 'dragonfaerie', is_admin: true, password: 'meowmeow', password_confirmation: 'meowmeow' )
 
-Horse.create!(make: 1, mold: 1, released_as: 'Zafira', breed: 3, color: 'Liver Chestnut', pattern: 'None', gender: 'Mare', finish: 'OF', size: 'Traditional', user_id: 1, location: 'Home Office', verified: true, office_pony: '', purchase_price: 175, condition: 'excellent' )
-Horse.create!(make: 1, mold: 1, released_as: 'RD Marciea Bey', breed: 3, color: 'Bay', pattern: 'None', gender: 'Mare', finish: 'OF', size: 'Traditional', user_id: 1, location: 'Living Room Shelves', verified: true, office_pony: '', purchase_price: 175, condition: 'excellent' )
+Horse.create!(make: 1, mold: 1, released_as: 'Zafira', breed: 3, color: 'Liver Chestnut', pattern: 'None', gender: 'Mare', finish: 'Original Finish', size: 'Traditional', user_id: 1, location: 'Home Office', verified: true, office_pony: '', purchase_price: 175, condition: 'Excellent' )
+Horse.create!(make: 1, mold: 1, released_as: 'RD Marciea Bey', breed: 3, color: 'Bay', pattern: 'None', gender: 'Mare', finish: 'Original Finish', size: 'Traditional', user_id: 1, location: 'Living Room Shelves', verified: true, office_pony: '', purchase_price: 175, condition: 'Excellent' )
 
 Manufacturer.create!(make: "Breyer")
 Manufacturer.create!(make: "Peter Stone")
