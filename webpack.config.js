fd66789c791c9c2b584908f82da3fa47135b0a4e
dragonfaerie <hope.m.ross@gmail.com>
2023-07-05 11:14:25 -0500

    tweaking away

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
-
 const stylesHandler = 'style-loader';
-
-
 
 const config = {
     entry: './src/index.ts',
@@ -19,6 +18,10 @@ const config = {
         host: 'localhost',
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+
         // Add your plugins here
         // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
@@ -31,7 +34,7 @@ const config = {
             },
             {
                 test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
+                use: [stylesHandler, 'css-loader', 'postcss-loader'],
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
@@ -44,6 +47,9 @@ const config = {
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
+        fallback: {
+            fs: false
+        }
     },
 };
 
@@ -51,6 +57,8 @@ module.exports = () => {
     if (isProduction) {
         config.mode = 'production';
         
+        
+        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
         
     } else {
         config.mode = 'development';
